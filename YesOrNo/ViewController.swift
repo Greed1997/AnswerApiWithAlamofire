5a38cd9e96eb96cd440c4c81481cf44fdee8bc4e
Greed1997 <43263569+Greed1997@users.noreply.github.com>
2022-09-28 18:27:46 +0300

    Initial Commit

---
+++ YesOrNo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YesOrNo
+//
+//  Created by Александр on 28.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
