79fc696f5803f5d3c0d1dcf8deaf0ba353b49bf5
Greed1997 <43263569+Greed1997@users.noreply.github.com>
2022-09-28 20:08:09 +0300

    some trouble

---
+++ YesOrNo/Models/Answer.swift
@@ -0,0 +1,10 @@
+//
+//  YesOrNoModel.swift
+//  YesOrNo
+//
+//  Created by Александр on 28.09.2022.
+//
+
+import Foundation
+
+struct 
---
+++ YesOrNo/Services/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  DataManager.swift
+//  YesOrNo
+//
+//  Created by Александр on 28.09.2022.
+//
+
+import Foundation
